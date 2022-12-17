ed14f2f3fa1f5bde4bcc716bfb2800689eb9b409
ardhanurazizah <naardha0@gmail.com>
2022-12-17 18:50:59 +0700

    Login Page

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:final_mobile/pages/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        "/": (context) => const LoginPage(),
+        // "/listpage": (context) => const ListPage(),
+      },
+      initialRoute: "/",
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
